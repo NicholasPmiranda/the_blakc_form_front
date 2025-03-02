805c1e622cc29bb4061d0f6b40afe8512415724d
nick <nicholas.miranda@sixchains.com.br>
2025-03-02 20:31:10 -0300

    refactor RespostaVisualizacaoForm for improved layout and responsiveness

--- src/components/admin/form/respostaForm/RespostaVisualizacaoForm.vue
+++ src/components/admin/form/respostaForm/RespostaVisualizacaoForm.vue
@@ -38,17 +38,17 @@ respostaStore.getResposta(route.params.id)
 
                     <h1 class="text-center text-2xl">Grupo Respostas</h1>
 
-                    <div class="w-5/6   flex tex items-center justify-center ">
+                    <div class="w-5/6 b   flex tex items-center justify-center ">
                         <div class="flex  w-full  flex-wrap ">
 
-                            <div  v-for="resposta  in respostaStore.grupo_ativo">
+                            <div class=" w-1/2"  v-for="resposta  in respostaStore.grupo_ativo">
                                 <div v-if="resposta.is_file == false"
-                                    class="  w-1/2 mt-8  border-l-8 hover:cursor-pointer hover:bg-dark-purple   rounded border-purple-default p-4">
+                                    class="   mt-8  border-l-8 hover:cursor-pointer hover:bg-dark-purple   rounded border-purple-default p-4">
                                     <p> {{ resposta.questao }}:</p>
                                     <p> {{ resposta.resposta }}</p>
                                 </div>
                                 <div v-else
-                                     class="  w-1/2 mt-8  border-l-8 hover:cursor-pointer hover:bg-dark-purple   rounded border-purple-default p-4">
+                                     class="  mt-8  border-l-8 hover:cursor-pointer hover:bg-dark-purple   rounded border-purple-default p-4">
                                     <p> {{ resposta.questao }}:</p>
 
                                     <Button size="small" icon="pi pi-download" as="a" label="Download" :href="resposta.resposta" target="_blank"  />
